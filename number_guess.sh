0209986f58a770c4b01326b0d12e56d3dbf27b2a
Jamesupback <jamessajan07@gmail.com>
2023-05-30 15:31:39 -0400

    feat:inserts new user into database

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=numguess -t --no-align -c"
 echo -e "\n~~~~ NUMBER GUESS GAME ~~~~"
-TARGET= $(($RANDOM % 1000 +1))
+TARGET=$(($RANDOM % 1000 +1))
 echo -e "\nEnter your username:"
 read USERNAME
+USER_NAME=$($PSQL "SELECT username from games WHERE username='$USERNAME' ")
+if [[ -z $USER_NAME ]]
+then
+echo Welcome, $USERNAME! It looks like this is your first time here.
+NEW_USER=$($PSQL "INSERT INTO games(username) VALUES('$USERNAME')")
+fi
+echo -e "\nGuess the secret number between 1 and 1000:"
+read guess
