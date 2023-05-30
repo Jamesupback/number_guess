1eaeeb1f32c59de3a4dff3d1eaa0d088cb8d509c
Jamesupback <jamessajan07@gmail.com>
2023-05-30 15:14:23 -0400

    feat:query variable added

--- number_guess.sh
+++ number_guess.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 echo -e "\n~~~~ NUMBER GUESS GAME ~~~~"
 TARGET= $(($RANDOM % 1000 +1))
 echo -e "\nEnter your username:"
